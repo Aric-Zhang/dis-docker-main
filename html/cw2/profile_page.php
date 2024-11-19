6ebf4c8c7f9fe9b648797b1876c71546e60da663
Zhang Zishi <america-california@qq.com>
2024-11-19 10:45:16 +0800

    The content of profile page

--- html/cw2/profile_page.php
+++ html/cw2/profile_page.php
@@ -18,6 +18,62 @@ if (!isset($_SESSION[USERNAME])) {
     <title>DIS Entrance Page</title>
     <style>
         @import "../css/dis_cw2_common.css";
+
+        .profile-container{
+            display: flex;
+            width: 100%;
+            max-width: 60em;
+            padding: 10px; /* 内边距 */
+
+        }
+        .profile-wrapper{
+            display: flex;
+            flex-direction: row;
+            width: 100%;
+            justify-content: center;
+        }
+        .left-column {
+            flex: 2; /* 左侧列的宽度为总宽度的三分之二 */
+            padding: 0;
+            box-sizing: border-box; /* 包括内边距和边框在内的宽度计算 */
+        }
+
+        .right-column {
+            flex: 3; /* 右侧列的宽度为总宽度的三分之一 */
+            padding: 0;
+            box-sizing: border-box; /* 包括内边距和边框在内的宽度计算 */
+        }
+        .profile_in_column_block{
+            margin: 1rem 0.5rem;
+            border: 1px solid #ccc;
+        }
+        .profile_in_block_column_wrapper{
+            display: flex;
+            flex-direction: column;
+            align-items: center
+        }
+        .profile_in_block_row_wrapper{
+            flex-direction: row;
+            width: 100%
+        }
+        .profile_user_icon{
+            width: 7rem;
+            height: 7rem;
+            font-size: 2rem;
+            border-radius: 50%;
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            cursor: pointer;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+            padding: 0;
+        }
+        .profile_in_block_spacing{
+            margin: 1.5rem;
+        }
     </style>
 </head>
 <body>
@@ -27,6 +83,42 @@ render_navi_bar(__FILE__);
 <?php
 ?>
 <div class="main_page_wrapper">
+    <div class="profile-wrapper">
+        <div class = 'profile-container'>
+            <div class="left-column">
+                <div class="profile_in_column_block">
+                    <div class="profile_in_block_spacing"></div>
+                    <div class="profile_in_block_column_wrapper">
+                        <div class="profile_in_block_row_wrapper">
+                        </div>
+                        <span class="profile_user_icon"><?php echo strtoupper($_SESSION[USERNAME][0]) ?></span>
+                        <div style="font-size: 2rem; margin-top: 1rem;">
+                            <?php echo $_SESSION['Username'] ?>
+                        </div>
+                        <div style="font-size: 1.2rem; color: dimgrey;">
+                            <?php
+                                $authority = $_SESSION[AUTHORITY];
+                                echo $authority;
+                            ?>
+                        </div>
+                        <div style="font-size: 1.2rem; color: dimgrey;">
+                            Other descriptions
+                        </div>
+                    </div>
+                    <div class="profile_in_block_spacing"></div>
+                </div>
+                <div class="profile_in_column_block">
+                    Left content
+                </div>
+            </div>
+            <div class="right-column">
+                <div class="profile_in_column_block">
+                    Right content
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
 </body>
 </html>
