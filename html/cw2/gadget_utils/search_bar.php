4b290f66674bac2bae234a03f90de1a32f939636
Zhang Zishi <america-california@qq.com>
2024-11-24 15:33:09 +0800

    add search bar gadget

---
+++ html/cw2/gadget_utils/search_bar.php
@@ -0,0 +1,159 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'].'/environment_constants.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/php_utils.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/'.GADGET_UTILS_DIR.'dropdown_menu.php';
+
+class SearchOptionRenderer{
+
+    var $invisible_input_id;
+    var $invisible_input_name;
+    var $search_opt_button_id;
+    var $search_opt_button_text_id;
+
+    var $dropdown_menu_item_array;
+
+    var $search_type_array;
+
+    var $dropdown_menu_id;
+    var $dropdown_button_id;
+
+    function set_parameters(
+        $invisible_input_id,
+        $invisible_input_name,
+        $search_opt_button_id,
+        $search_opt_button_text_id,
+
+        $dropdown_menu_item_type_placeholder_array,
+
+        $dropdown_menu_id,
+        $dropdown_button_id){
+
+        $this->invisible_input_id = $invisible_input_id;
+        $this->invisible_input_name = $invisible_input_name;
+        $this->search_opt_button_id = $search_opt_button_id;
+        $this->search_opt_button_text_id = $search_opt_button_text_id;
+
+        $dropdown_menu_item_array = array();
+        $search_type_array = array();
+        foreach ($dropdown_menu_item_type_placeholder_array as $dmitp){
+            array_push($dropdown_menu_item_array, array('text'=> $dmitp['text'], 'href'=>'#','id'=>$dmitp['id']));
+            array_push($search_type_array, array('id'=>$dmitp['id'],'type'=>$dmitp['type'],'func'=>$dmitp['func']));
+        }
+
+        $this->dropdown_menu_item_array = $dropdown_menu_item_array;
+
+        $this->search_type_array = $search_type_array;
+
+        $this->dropdown_menu_id = $dropdown_menu_id;
+        $this->dropdown_button_id = $dropdown_button_id;
+    }
+    function render(){
+
+        $invisible_input_id = $this->invisible_input_id;
+        $invisible_input_name = $this->invisible_input_name;
+        $search_opt_button_id = $this->search_opt_button_id;
+        $search_opt_button_text_id = $this->search_opt_button_text_id;
+
+        $dropdown_menu_item_array=$this->dropdown_menu_item_array;
+
+        $search_type_array=$this->search_type_array;
+
+        $dropdown_menu_id = $this->dropdown_menu_id;
+        $dropdown_button_id = $this->dropdown_button_id;
+
+        $links_string = "";
+        $links_additional_functions_string = "";
+        foreach ($search_type_array as $search_type) {
+            $links_string.="[document.getElementById('".$search_type['id']."'),'".$search_type['type']."'],";
+            $links_additional_functions_string.= $search_type['func'];
+        }
+
+        $search_default_type = "";
+        if(count($search_type_array)>0){
+            $search_default_type = $search_type_array[0]['type'];
+        }
+        $button_default_text = "";
+        if(count($dropdown_menu_item_array)>0){
+            $button_default_text = $dropdown_menu_item_array[0]['text'];
+        }
+        $start_search_opt_button_doc = <<<EOT
+                <input id="$invisible_input_id" type="text" name="$invisible_input_name" style="display: none" value="$search_default_type">
+                <button id="$search_opt_button_id" class="btn btn-primary form_control_search dropdown_button" type="button">
+                    <span id="$search_opt_button_text_id">$button_default_text</span>
+EOT;
+        $bind_function_script_doc=<<<EOT
+                                    <script>
+                        document.addEventListener('DOMContentLoaded', function() {
+                            const links = [
+                                $links_string
+                            ];
+                            
+                            const link_function_map = new Map();
+                            $links_additional_functions_string                    
+
+                            var span = document.getElementById('$search_opt_button_text_id');
+                            var input = document.getElementById('$invisible_input_id');
+
+                            links.forEach((item)=>{
+                                var link = item[0];
+                                var type = item[1];
+                                link.addEventListener('click', function(event) {
+                                    event.preventDefault();
+                                    span.innerHTML = link.textContent;
+                                    input.value = type;
+                                    var additional_func = link_function_map.get(link.id);
+                                    if(additional_func){
+                                        additional_func(event);
+                                    }
+                                });
+                            });
+                        });
+                    </script>
+EOT;
+
+        $end_search_opt_button_doc = <<<EOT
+                </button>
+EOT;
+        echo $start_search_opt_button_doc;
+
+        render_dropdown_menu($dropdown_menu_id, $dropdown_menu_item_array);
+        bind_dropdown_menu_to_button($dropdown_menu_id, $dropdown_button_id);
+
+        echo $bind_function_script_doc;
+        echo $end_search_opt_button_doc;
+    }
+}
+function start_search_bar(){
+    $start_search_bar_doc = <<<EOT
+        <form action="" class="general_form" method="get">
+            <div class="form-group search_input_wrapper">
+EOT;
+    echo $start_search_bar_doc;
+}
+function end_search_bar(){
+    $end_search_bar_doc = <<<EOT
+            </div>
+        </form>
+EOT;
+    echo $end_search_bar_doc;
+}
+function render_search_input_and_button($input_id,$input_name,$placeholder)
+{
+    $search_input_doc = <<<EOT
+                    <input id='$input_id' type="text" name="$input_name" class="form-control form_control_search search_input" placeholder="$placeholder">
+EOT;
+    $search_button_doc = <<<EOT
+                    <button class="btn btn-primary form_control_search search_button" type="submit">Search</button>
+EOT;
+
+    echo $search_input_doc;
+    echo $search_button_doc;
+}
+function javascript_replace_placeholder_string($link_id, $input_id, $placeholder){
+    return "link_function_map.set('".$link_id."', function(event){                                        
+                                    var search_input = document.getElementById('".$input_id."');
+                                    search_input.placeholder = '".$placeholder."';
+                                    });";
+}
+
+?>
--- html/cw2/search_people_page.php
+++ html/cw2/search_people_page.php
@@ -4,6 +4,7 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/php_utils.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/'.GADGET_UTILS_DIR.'navi_bar.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/'.GADGET_UTILS_DIR.'grid_container.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/'.GADGET_UTILS_DIR.'dropdown_menu.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/'.GADGET_UTILS_DIR.'search_bar.php';
 
 session_start();
 if (!isset($_SESSION[USERNAME])) {
@@ -106,182 +107,43 @@ render_navi_bar(__FILE__);
 ?>
 <div class="main_page_wrapper">
     <div>
-        <form action="" class="general_form" method="get">
-            <div class="form-group search_input_wrapper">
+        <?php
 
-                <?php
-                class SearchOptionRenderer{
-                    var $search_input_id;
-                    var $invisible_input_id;
-                    var $invisible_input_name;
-                    var $search_opt_button_id;
-                    var $search_opt_button_text_id;
+        $search_people_renderer = new SearchOptionRenderer();
 
-                    var $dropdown_menu_item_array;
+        $link_id_1 = "search_type_general";
+        $link_id_2 = "search_type_name";
+        $link_id_3 = "search_type_driving_license";
+        $text_1 = "General Search";
+        $text_2 = "Search Name";
+        $text_3 = "Search Driving Licence";
+        $type_1 = "general";
+        $type_2 = "name";
+        $type_3 = "driving_license";
+        $placeholder_1 = "Type in people\'s name or driving licence number";
+        $placeholder_2 = "Type in people\'s name";
+        $placeholder_3 = "Type in people\'s driving license number";
 
-                    var $search_type_array;
+        $search_input_id = "search_people_input";
 
-                    var $dropdown_menu_id;
-                    var $dropdown_button_id;
-
-                    function set_parameters($search_input_id,
-                    $invisible_input_id,
-                    $invisible_input_name,
-                    $search_opt_button_id,
-                    $search_opt_button_text_id,
-
-                    $dropdown_menu_item_array,
-                    //$dropdown_menu_item_type_placeholder_array,
-                    $search_type_array,
-
-                    $dropdown_menu_id,
-                    $dropdown_button_id){
-                        $this->search_input_id = $search_input_id;
-                        $this->invisible_input_id = $invisible_input_id;
-                        $this->invisible_input_name = $invisible_input_name;
-                        $this->search_opt_button_id = $search_opt_button_id;
-                        $this->search_opt_button_text_id = $search_opt_button_text_id;
-
-                        //$dropdown_menu_item_array = array();
-                        //$search_type_array = array();
-                        //foreach ($dropdown_menu_item_type_placeholder_array as $dmitp){
-                            //array_push($dropdown_menu_item_array, array('text'=> $dmitp['text'], 'href'=>'#','id'=>$dmitp['id']));
-                            //array_push($search_type_array, array('id'=>$dmitp['id'],'type'=>$dmitp['type'],'func'=>$dmitp['func']));
-                        //}
-
-                        $this->dropdown_menu_item_array = $dropdown_menu_item_array;
-
-                        $this->search_type_array = $search_type_array;
-
-                        $this->dropdown_menu_id = $dropdown_menu_id;
-                        $this->dropdown_button_id = $dropdown_button_id;
-                    }
-                    function render(){
-                        $search_input_id = $this->search_input_id;
-                        $invisible_input_id = $this->invisible_input_id;
-                        $invisible_input_name = $this->invisible_input_name;
-                        $search_opt_button_id = $this->search_opt_button_id;
-                        $search_opt_button_text_id = $this->search_opt_button_text_id;
-
-                        $dropdown_menu_item_array=$this->dropdown_menu_item_array;
-
-                        $search_type_array=$this->search_type_array;
-
-                        $dropdown_menu_id = $this->dropdown_menu_id;
-                        $dropdown_button_id = $this->dropdown_button_id;
-
-                        $links_string = "";
-                        $links_additional_functions_string = "";
-                        foreach ($search_type_array as $search_type) {
-                            $links_string.="[document.getElementById('".$search_type['id']."'),'".$search_type['type']."'],";
-                            $links_additional_functions_string.= $search_type['func'];
-                        }
-
-                        $search_default_type = "";
-                        if(count($search_type_array)>0){
-                            $search_default_type = $search_type_array[0]['type'];
-                        }
-                        $button_default_text = "";
-                        if(count($dropdown_menu_item_array)>0){
-                            $button_default_text = $dropdown_menu_item_array[0]['text'];
-                        }
-                        $start_search_opt_button_doc = <<<EOT
-                <input id="$invisible_input_id" type="text" name="$invisible_input_name" style="display: none" value="$search_default_type">
-                <button id="$search_opt_button_id" class="btn btn-primary form_control_search dropdown_button" type="button">
-                    <span id="$search_opt_button_text_id">$button_default_text</span>
-EOT;
-                        $bind_function_script_doc=<<<EOT
-                                    <script>
-                        document.addEventListener('DOMContentLoaded', function() {
-                            const links = [
-                                $links_string
-                            ];
-                            
-                            const link_function_map = new Map();
-                            $links_additional_functions_string                    
-
-                            var span = document.getElementById('$search_opt_button_text_id');
-                            var input = document.getElementById('$invisible_input_id');
-
-                            links.forEach((item)=>{
-                                var link = item[0];
-                                var type = item[1];
-                                link.addEventListener('click', function(event) {
-                                    event.preventDefault();
-                                    span.innerHTML = link.textContent;
-                                    input.value = type;
-                                    var additional_func = link_function_map.get(link.id);
-                                    if(additional_func){
-                                        additional_func(event);
-                                    }
-                                });
-                            });
-                        });
-                    </script>
-EOT;
-
-                        $end_search_opt_button_doc = <<<EOT
-                </button>
-EOT;
-                        echo $start_search_opt_button_doc;
-
-                        render_dropdown_menu($dropdown_menu_id, $dropdown_menu_item_array);
-                        bind_dropdown_menu_to_button($dropdown_menu_id, $dropdown_button_id);
-
-                        echo $bind_function_script_doc;
-                        echo $end_search_opt_button_doc;
-                    }
-                }
-
-                $search_people_renderer = new SearchOptionRenderer();
-
-                function javascript_replace_placeholder_string($link_id, $input_id, $placeholder){
-                    return "link_function_map.set('".$link_id."', function(event){                                        
-                                    var search_input = document.getElementById('".$input_id."');
-                                    search_input.placeholder = '".$placeholder."';
-                                    });";
-                }
-
-                $link_id_1 = "search_type_general";
-                $link_id_2 = "search_type_name";
-                $link_id_3 = "search_type_driving_license";
-                $text_1 = "General Search";
-                $text_2 = "Search Name";
-                $text_3 = "Search Driving Licence";
-                $type_1 = "general";
-                $type_2 = "name";
-                $type_3 = "driving_license";
-                $placeholder_1 = "Type in people\'s name or driving licence number";
-                $placeholder_2 = "Type in people\'s name";
-                $placeholder_3 = "Type in people\'s driving license number";
-
-
-                $search_people_renderer->set_parameters($search_input_id = "search_people_input",
-                    $invisible_input_id = "search_people_type_input",
-                    $invisible_input_name = "search_people_type",
-                    $search_opt_button_id = "dropdown-button-search-type-people",
-                    $search_opt_button_text_id = "dropdown-button-search-type-people-text",
-                    $dropdown_menu_item_array=array(
-                        array("text"=>$text_1, "href"=>"#", "id"=>$link_id_1),
-                        array("text"=>$text_2, "href"=>"#", "id"=>$link_id_2),
-                        array("text"=>$text_3, "href"=>"#", "id"=>$link_id_3),
-                    ),
-                    $search_type_array=array(
-                        array("id"=>$link_id_1, "type"=>$type_1, "func"=>javascript_replace_placeholder_string($link_id_1, $search_input_id, $placeholder_1)),
-                        array("id"=>$link_id_2, "type"=>$type_2, "func"=>javascript_replace_placeholder_string($link_id_2, $search_input_id, $placeholder_2)),
-                        array("id"=>$link_id_3, "type"=>$type_3, "func"=>javascript_replace_placeholder_string($link_id_3, $search_input_id, $placeholder_3)),
-                    ),
-                    $dropdown_menu_id = "dropdown-menu-search-type-people",
-                    $dropdown_button_id = "dropdown-button-search-type-people"
-                );
-
-                $search_people_renderer->render();
-
-                ?>
-                <input id="search_people_input" type="text" name="search_people_text" class="form-control form_control_search search_input" placeholder="Type in people's name or driving licence number">
-                <button class="btn btn-primary form_control_search search_button" type="submit">Search</button>
-            </div>
-        </form>
+        $search_people_renderer->set_parameters(
+            $invisible_input_id = "search_people_type_input",
+            $invisible_input_name = "search_people_type",
+            $search_opt_button_id = "dropdown-button-search-type-people",
+            $search_opt_button_text_id = "dropdown-button-search-type-people-text",
+            $dropdown_menu_item_type_placeholder_array = array(
+                array('id'=>$link_id_1, 'text'=>$text_1, 'type'=>$type_1, 'func'=>javascript_replace_placeholder_string($link_id_1, $search_input_id, $placeholder_1)),
+                array('id'=>$link_id_2, 'text'=>$text_2, 'type'=>$type_2, 'func'=>javascript_replace_placeholder_string($link_id_2, $search_input_id, $placeholder_2)),
+                array('id'=>$link_id_3, 'text'=>$text_3, 'type'=>$type_3, 'func'=>javascript_replace_placeholder_string($link_id_3, $search_input_id, $placeholder_3))
+            ),
+            $dropdown_menu_id = "dropdown-menu-search-type-people",
+            $dropdown_button_id = "dropdown-button-search-type-people"
+        );
+        start_search_bar();
+        $search_people_renderer->render();
+        render_search_input_and_button($search_input_id, "search_people_text", "Type in people's name or driving licence number");
+        end_search_bar();
+        ?>
     </div>
     <div class="search_page_wrapper">
         <hr>
