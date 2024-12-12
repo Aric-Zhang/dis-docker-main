611662d6eb81bf3b8b21a0fc2703c40a8e82b3d8
Zhang Zishi <america-california@qq.com>
2024-12-13 00:10:21 +0800

    Update the dashboard icons

--- html/cw2/navi_page.php
+++ html/cw2/navi_page.php
@@ -34,13 +34,13 @@ render_navi_bar(__FILE__);
         array("icon_file_name"=>'person_icon.png',"text"=>'Search Person',"href"=>"search_people_page.php"),
         array("icon_file_name"=>'vehicles_icon.png',"text"=>'Search Vehicle',"href"=>"search_vehicle_page.php"),
         array("icon_file_name"=>'traffic_violation.png',"text"=>'Search Incident',"href"=>"search_incident_page.php"),
-        array("icon_file_name"=>'person_icon.png',"text"=>'Add or Update Vehicle',"href"=>"add_update_vehicle_page.php"),
-        array("icon_file_name"=>'person_icon.png',"text"=>'Report Incident',"href"=>"report_incident.php")
+        array("icon_file_name"=>'add_vehicle_icon.png',"text"=>'Add or Update Vehicle',"href"=>"add_update_vehicle_page.php"),
+        array("icon_file_name"=>'report_icon.png',"text"=>'Report Incident',"href"=>"report_incident.php")
     ) ;
     if(isset($_SESSION[AUTHORITY]) && $_SESSION[AUTHORITY] == AUTHORITY_ADMIN ){
         array_push($grid_item_info_array,
-            array("icon_file_name"=>'person_icon.png',"text"=>'Create New Account',"href"=>"create_new_account_page.php"),
-            array("icon_file_name"=>'person_icon.png',"text"=>'Audit Trail',"href"=>"search_modification_page.php"));
+            array("icon_file_name"=>'create_new_account_icon.png',"text"=>'Create New Account',"href"=>"create_new_account_page.php"),
+            array("icon_file_name"=>'audit_trail_icon.png',"text"=>'Audit Trail',"href"=>"search_modification_page.php"));
     }
     render_grid(__FILE__, $grid_item_info_array);
 ?>
